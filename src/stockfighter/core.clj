90c7a4afcedabb5da1a77e69c694073d6ed1cff0
Angus Fletcher <ethdem@gmail.com>
2015-12-29 19:57:23 -0400

    Add gm api, remove environ

--- src/stockfighter/core.clj
+++ src/stockfighter/core.clj
@@ -1,83 +1,63 @@
 (ns stockfighter.core
-  (:require [environ.core :as e]
-            [clj-http.client :as client]
-            [cheshire.core :as json]
+  (:require [clj-http.client :as client]
+            [clj-http.conn-mgr :as cm]
             [aleph.http :as http]
             [manifold.stream :as s]
             [clojure.core.async :as a]))
 
-(def api-key (e/env :api-key))
-
 (defn common-opts []
-  {:as :json
-   :headers {"X-Starfighter-Authorization" api-key}})
+  {})
 
-(defn api-up? []
-  (let [response (client/get "https://api.stockfighter.io/ob/api/heartbeat" (common-opts))]
-    (get-in response [:body :ok])))
+(defmacro def-api-action [name args verb uri opts]
+  `(defn ~name
+     [~@args]
+     (client/request (merge ~opts {:method ~verb
+                                   :url (format ~uri ~@args)
+                                   :as :json}))))
 
-(defn venue-up? [venue]
-  (let [uri (format "https://api.stockfighter.io/ob/api/venues/%s/heartbeat" venue)
-        response (client/get uri (common-opts))]
-    (get-in response [:body :ok])))
+(defmacro def-api-with-body [name [body & args] uri opts]
+  `(defn ~name
+     [~@args ~body]
+     (client/request (merge ~opts {:method :post
+                                   :url (format ~uri ~@args)
+                                   :as :json
+                                   :content-type :json
+                                   :form-params ~body}))))
 
-(defn get-stocks [venue]
-  (let [uri (format "https://api.stockfighter.io/ob/api/venues/%s/stocks" venue)
-        response (client/get uri (common-opts))]
-    (if (get-in response [:body :ok])
-      (get-in response [:body :symbols])
-      nil)))
-
-(defn get-book [venue stock]
-  (let [uri (format "https://api.stockfighter.io/ob/api/venues/%s/stocks/%s" venue stock)
-        response (client/get uri (common-opts))]
-    (if (get-in response [:body :ok])
-      (get-in response [:body :bids]))))
-
-(defn make-order [venue stock payload]
-  (let [uri (format "https://api.stockfighter.io/ob/api/venues/%s/stocks/%s/orders" venue stock)
-        opts (-> (common-opts)
-                 (assoc :form-params (merge payload {:venue venue
-                                                     :stock stock}))
-                 (assoc :content-type :json))
-        response (client/post uri opts)]
-    (get response :body)))
-
-(defn get-quote [venue stock]
-  (let [uri (format "https://api.stockfighter.io/ob/api/venues/%s/stocks/%s/quote" venue stock)
-        response (client/get uri (common-opts))]
-    (get response :body)))
-
-(defn orders-by-account [venue account]
-  (let [uri (format "https://api.stockfighter.io/ob/api/venues/%s/accounts/%s/orders" venue account)
-        response (client/get uri (common-opts))]
-    (get response :body)))
-
-(defn orders-by-stock-and-account [venue account stock]
-  (let [uri (format "https://api.stockfighter.io/ob/api/venues/%s/accounts/%s/stocks/%s/orders"
-                    venue
-                    account
-                    stock)
-        response (client/get uri (common-opts))]
-    (get response :body)))
-
-(defn get-order-status [venue account stock order-id]
-  (let [uri (format "https://api.stockfighter.io/ob/api/venues/%s/stocks/%s/orders/%s"
-                    venue
-                    account
-                    stock
-                    order-id)
-        response (client/get uri (common-opts))]
-    (get response :body)))
-
-(defn cancel-order [venue account stock order-id]
-  (let [uri (format "https://api.stockfighter.io/ob/api/venues/%s/stocks/%s/orders/%s"
-                    venue
-                    account
-                    stock
-                    order-id)
-        response (client/get uri (common-opts))]
-    (get response :body)))
+(defn init [api-key mgr-opts]
+  (let [mgr (cm/make-reusable-conn-manager (or mgr-opts {}))
+        common-opts {:connection-manager mgr
+                     :as :json
+                     :content-type :json
+                     :headers {"X-Starfighter-Authorization" api-key}}]
+    (def-api-action api-up? [] :get "https://api.stockfighter.io/ob/api/heartbeat"
+      common-opts)
+    (def-api-action venue-up? [venue] :get "https://api.stockfighter.io/ob/api/venues/%s/heartbeat"
+      common-opts)
+    (def-api-action get-stocks [venue] :get "https://api.stockfighter.io/ob/api/venues/%s/stocks"
+      common-opts)
+    (def-api-action get-book [venue stock] :get "https://api.stockfighter.io/ob/api/venues/%s/stocks/%s"
+      common-opts)
+    (def-api-action get-quote [venue stock] :get "https://api.stockfighter.io/ob/api/venues/%s/stocks/%s/quote"
+      common-opts)
+    (def-api-action get-order [venue stock order] :get "https://api.stockfighter.io/ob/api/venues/%s/stocks/%s/orders/%s"
+      common-opts)
+    (def-api-action cancel-order [venue stock order] :delete "https://api.stockfighter.io/ob/api/venues/%s/stocks/%s/orders/%s"
+      common-opts)
+    (def-api-action all-orders [venue account] :get "https://api.stockfighter.io/ob/api/venues/accounts/%s/orders"
+      common-opts)
+    (def-api-action orders-by-stock [venue account stock] :get "https://api.stockfighter.io/ob/api/venues/accounts/%s/orders"
+      common-opts)
+    (def-api-with-body post-order [body venue stock] "https://api.stockfighter.io/ob/api/venues/%s/stocks/%s/orders"
+      common-opts)
+    (def-api-action get-level [level] :post "https://www.stockfighter.io/gm/levels/%s"
+      common-opts)
+    (def-api-action stop-level [instance] :post "https://www.stockfighter.io/gm/instances/%s/stop"
+      common-opts)
+    (def-api-action resume-level [instance] :post "https://www.stockfighter.io/gm/instances/%s/resume"
+      common-opts)
+    (def-api-action get-instance [instance] :get "https://www.stockfighter.io/gm/instances/%s"
+      common-opts)))
 
 (defn quotes [account venue]
   (let [uri (format "wss://api.stockfighter.io/ob/api/ws/%s/venues/%s/tickertape" account venue)
@@ -85,7 +65,7 @@
     (if-let [socket (try @(http/websocket-client uri)
                          (catch Exception e (.printStackTrace e)
                                 e))]
-      (do (s/connect (s/map json/parse-string  socket) quotes>)
+      (do (s/connect (s/map identity socket) quotes>)
           {:chan quotes>
            :close (delay (.close socket)
                          (a/close! quotes>))})
@@ -96,9 +76,10 @@
         fills> (a/chan 1024)]
     (if-let [socket (try @(http/websocket-client uri)
                          (catch Exception e (.printStacktrace e)
+                                (println (.getMessage e))
                                 e))]
-      (do (println socket)
-          (s/connect (s/map json/parse-string socket) fills>)
+      (do (s/connect (s/map identity socket) fills>)
           {:chan fills>
            :close (delay (.close socket)
-                         (a/close! fills>))}))))
+                         (a/close! fills>))})
+      (println "Nope"))))
